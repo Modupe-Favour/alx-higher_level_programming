ab841aa4f62d834899e3b33d4c332d74c10d83fa
Modupe <modups05@gmail.com>
2023-05-15 17:25:33 -0700

    update

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,8 +2,8 @@
 # 1-element_at.py
 
 
-def retrieve_my_element(my_list, num):
+def element_at(my_list, idx):
     """Retrive an element from a list."""
-    if num < 0 or num > (len(my_list) - 1):
+    if idx < 0 or idx > (len(my_list) - 1):
         return None
-    return (my_list[num])
+    return (my_list[idx])
--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,8 +2,8 @@
 # 2-replace_in_list.py
 
 
-def replace_element(my_list, num1, num2):
+def replace_in_list(my_list, idx, element):
     """Replace an element of a list at a specific position."""
-    if num1 >= 0 and num1 < len(my_list):
-        my_list[num1] = num2
+    if idx >= 0 and idx < len(my_list):
+        my_list[idx] = element
     return (my_list)
