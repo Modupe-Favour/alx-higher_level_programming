cb27c4e7765746573757d4ae4ee4552fd0e8d892
Modupe <modups05@gmail.com>
2023-05-15 17:18:34 -0700

    update

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,9 +2,9 @@
 # 3-print_reversed_list_integer.py
 
 
-def reversed_list(my_list=[]):
+def print_reversed_list_integer(my_list=[]):
     """Print all integers of a list in reverse order."""
     if isinstance(my_list, list):
         my_list.reverse()
-        for n in my_list:
-            print("{:d}".format(n))
+        for i in my_list:
+            print("{:d}".format(i))
--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,11 +2,12 @@
 # 4-new_in_list.py
 
 
-def new_item_in_list(my_list, num1, num2):
+def new_in_list(my_list, idx, element):
     """Replace an element in a copied list at a specific position."""
-    if num1 < 0 or num2 > (len(my_list) - 1):
+    if idx < 0 or idx > (len(my_list) - 1):
         return (my_list)
 
     copy = [x for x in my_list]
-    copy[num1] = num2
+    copy[idx] = element
     return (copy)
+
